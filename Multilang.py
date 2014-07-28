83f0112f579ca55091a79e4d6e1b25239d96b8d5
TieSoul <tdbaksteen@hotmail.nl>
2014-07-28 21:12:51 +0200

    Added Random

--- Multilang.py
+++ Multilang.py
@@ -1,11 +1,12 @@
-import befunge, brainfuck, replacefuck, eitherfuck
+import befunge, brainfuck, replacefuck, eitherfuck, rand as random
 
 print("""
 +-----------------------------------+
 |     Multilang esoteric shell      |
 |            by TieSoul             |
 |  Replacefuck (rf), Befunge (bf),  |
-|  Brainfuck (b), Eitherfuck (ef)   |
+|  Brainfuck (b), Eitherfuck (ef),  |
+|             Random (r)            |
 |           are supported.          |
 +-----------------------------------+"""
 )
@@ -17,6 +18,7 @@ while True:
         bfcode = ''
         bcode = ''
         rfcode = ''
+        rcode = ''
         if a == 'ef':
             while True:
                 try:
@@ -29,7 +31,7 @@ while True:
                     except KeyboardInterrupt:
                         break
                     except:
-                        print("An error occurrec.")
+                        print("An error occurred.")
         elif a[:3] == 'ef ':
             efcode = a[3:]
         if a == 'bf':
@@ -44,7 +46,7 @@ while True:
                     except KeyboardInterrupt:
                         break
                     except:
-                        print("An error occurrec.")
+                        print("An error occurred.")
         elif a[:3] == 'bf ':
             bfcode = a[3:]
         if a == 'b':
@@ -59,7 +61,7 @@ while True:
                     except KeyboardInterrupt:
                         break
                     except:
-                        print("An error occurrec.")
+                        print("An error occurred.")
         elif a[:2] == 'b ':
             bcode = a[2:]
         if a == 'rf':
@@ -74,7 +76,22 @@ while True:
                     except KeyboardInterrupt:
                         break
                     except:
-                        print("An error occurrec.")
+                        print("An error occurred.")
+        if a == 'r':
+            while True:
+                try:
+                    rcode += input('r> ')
+                except KeyboardInterrupt:
+                    break
+                except:
+                    try:
+                        input()
+                    except KeyboardInterrupt:
+                        break
+                    except:
+                        print("An error occurred.")
+        elif a[:2] == 'r ':
+            rcode = a[2:]
         elif a[:4] == 'exit':
             break
         elif a[:4] == 'help':
@@ -86,7 +103,9 @@ b: enter brainfuck environment
 rf: enter replacefuck environment
 ef <eitherfuck code>: execute eitherfuck code
 ef: enter eitherfuck environment
-ctrl+C: exit an environment
+r <random code>: execute random code
+r: enter random environment
+ctrl+C: exit an environment and execute code
 exit: exit the shell""")
         if efcode != '':
             eitherfuck.execute(efcode)
@@ -99,6 +118,9 @@ exit: exit the shell""")
             print()
         if rfcode != '':
             replacefuck.execute(rfcode)
+            print()
+        if rcode != '':
+            random.execute(rcode)
             print()
     except KeyboardInterrupt:
         pass
---
+++ rand.py
@@ -0,0 +1,51 @@
+def execute(string):
+    string = list(string)
+    loopcount = 0
+    i = 0
+    while i in range(len(string)):
+        if string[i] not in '<>[]':
+            string.append(string[i])
+        elif string[i] == '>':
+            try:
+                if string[i+1] == '<':
+                    print(string.pop(), end="")
+                    i += 1
+                else:
+                    string.append(string.pop(0))
+                    i -= 1
+            except IndexError:
+                string.append(string.pop(0))
+                i -= 1
+        elif string[i] == '<':
+            string = [string.pop()] + string
+            i += 1
+        elif string[i] == '[':
+            if string[0] == string[-1]:
+                j = i + 1
+                loops = 1
+                while j in range(len(string)):
+                    if string[j] == '[':
+                        loops += 1
+                    elif string[j] == ']':
+                        loops -= 1
+                        if loops == 0:
+                            break
+                else:
+                    return
+                i = j
+        elif string[i] == ']':
+            if string[0] != string[-1]:
+                j = i - 1
+                loops = 1
+                while j in range(len(string)):
+                    if string[j] == '[':
+                        loops -= 1
+                        if loops == 0:
+                            break
+                    elif string[j] == ']':
+                        loops += 1
+                    j -= 1
+                else:
+                    return
+                i = j
+        i += 1
